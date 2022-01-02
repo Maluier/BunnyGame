624a04eb08b580c9401c4ba0844993215d3359d1
Maluier <96492979+Maluier@users.noreply.github.com>
2022-01-02 11:41:44 +0100

    animations

--- BunnyGame/scenes/towers/projectile_cat_turret.gd
+++ BunnyGame/scenes/towers/projectile_cat_turret.gd
@@ -2,6 +2,7 @@ extends "res://scenes/towers/hitbox.gd"
 
 export(int) var _speed := 16
 export(Vector2) var _direction := Vector2.LEFT
+onready var randomizer_cat_look = rand_range(0, 100)
 
 func _physics_process(delta: float) -> void:
 	position += _speed * _direction * delta
@@ -13,3 +14,9 @@ func _on_projectile_cat_turret_area_entered(area: Area2D) -> void:
 
 func _on_Timer_timeout() -> void:
 	$Sprite.visible = true
+	if randomizer_cat_look >= 66:
+		$Sprite.play("var1")
+	elif randomizer_cat_look <= 33:
+		$Sprite.play("var2")
+	else:
+		$Sprite.play("var3")
