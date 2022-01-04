693b6a23e478483ac051c6e0e4edf229fc93df3b
Xascoria <59613709+Xascoria@users.noreply.github.com>
2022-01-05 03:12:12 +0800

    Restrict tower to ground level

--- BunnyGame/scenes/levels/TurrentGrid.gd
+++ BunnyGame/scenes/levels/TurrentGrid.gd
@@ -9,14 +9,14 @@ var tower_dictionary := {}
 func _ready():	
 	pass
 	
-func _process(delta):
+func _process(_delta):
 	if current_preview_tower != null:
 		var local_mouse = get_local_mouse_position()
 		var tower_coord = self.world_to_map(local_mouse)
 		var preview_pos = self.map_to_world( tower_coord ) + TOWER_OFFSET
 		current_preview_tower.position = preview_pos
 		if Input.is_mouse_button_pressed( BUTTON_LEFT ):
-			if not tower_coord in tower_dictionary:
+			if not tower_coord in tower_dictionary and tower_coord[1] == 4:
 				tower_dictionary[tower_coord] = current_preview_tower
 				current_preview_tower.set_preview(false)
 				current_preview_tower.connect("disable_cat_turret", self, "_erase_building_slots")
@@ -25,6 +25,9 @@ func _process(delta):
 			else:
 				current_preview_tower.queue_free()
 			current_preview_tower = null
+
+func remove_preview():
+	current_preview_tower = null
 
 func _erase_building_slots(current_position): 
 	tower_dictionary.erase(current_position)
